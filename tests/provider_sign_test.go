e501de9c9aa2a71b6cfae24c87b711261884c477
Mike <manhlx.300695@gmail.com>
2019-07-05 15:21:02 +0700

    Update const for tests

--- tests/provider_sign_test.go
+++ tests/provider_sign_test.go
@@ -23,14 +23,18 @@ const (
 	normalAddress                  = "0x11c93c29591ba613852ac2c9278faec2d7e7ea59"
 	senderPK                       = "112CD7FA616EF6499DA9FA0A227AC73B4B109CC3F7F94C2BEFB3346CCB18CD08"
 	senderAddrStr                  = "0xa091e44e0B6Adc71ce1f58B81337343597301FF6"
-	contractAddrStrWithoutProvider = "0xA014d882aA3bd232c96c2AacbCCEcb334eE48B5b"
-	contractAddrStrWithProvider    = "0x335F7d696E35ADf35eE9209C6b3751Fad0c71C66"
+	contractAddrStrWithoutProvider = "0x0C53C92701a897BC7FAcfc6fa3bB01bEb8459F5E"
+	contractAddrStrWithProvider    = "0xc73f6b67fdeE628331d51C783a580804229b8eB1"
 	providerPK                     = "E6CFAAD68311D3A873C98750F52B2543F2C3C692A8F11E6B411B390BCD807133"
 	invadlidProviderPK             = "5564a4ddd059ba6352aae637812ea6be7d818f92b5aff3564429478fcdfe4e8a"
 	providerAddrStr                = "0x8359d8C955DAef81e171C13659bA3Fb0dDa144b4"
 
+	payload = "0x6060604052341561000f57600080fd5b6102e38061001e6000396000f30060606040526004361061004c576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063368b877214610051578063ce6d41de146100ae575b600080fd5b341561005c57600080fd5b6100ac600480803590602001908201803590602001908080601f0160208091040260200160405190810160405280939291908181526020018383808284378201915050505050509190505061013c565b005b34156100b957600080fd5b6100c1610156565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156101015780820151818401526020810190506100e6565b50505050905090810190601f16801561012e5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b80600090805190602001906101529291906101fe565b5050565b61015e61027e565b60008054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156101f45780601f106101c9576101008083540402835291602001916101f4565b820191906000526020600020905b8154815290600101906020018083116101d757829003601f168201915b5050505050905090565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061023f57805160ff191683800117855561026d565b8280016001018555821561026d579182015b8281111561026c578251825591602001919060010190610251565b5b50905061027a9190610292565b5090565b602060405190810160405280600081525090565b6102b491905b808211156102b0576000816000905550600101610298565b5090565b905600a165627a7a723058208a6eba9352e080994bc6a1041d71eff20de6686dbafb2341e23c07d938e706d60029"
+
 	testGasLimit   = 1000000
-	ethRPCEndpoint = "http://localhost:9015"
+	testGasPrice   = 1000000000
+	testAmountSend = 1000000
+	ethRPCEndpoint = "http://localhost:8545"
 )
 
 /*
@@ -51,7 +55,7 @@ func TestSendToNormalAddress(t *testing.T) {
 	gasPrice, err := ethClient.SuggestGasPrice(context.Background())
 	assert.NoError(t, err)
 
-	transaction := types.NewTransaction(nonce, normalAddr, big.NewInt(1000000), 1000000, gasPrice, nil)
+	transaction := types.NewTransaction(nonce, normalAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, nil)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	err = ethClient.SendTransaction(context.Background(), transaction)
 	assert.NoError(t, err)
@@ -75,7 +79,7 @@ func TestSendToNonEnterpriseSmartContractWithoutProviderSignature(t *testing.T) 
 	gasPrice, err := ethClient.SuggestGasPrice(context.Background())
 	assert.NoError(t, err)
 
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), 1000000, gasPrice, nil)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, nil)
 	// return newTransaction(nonce, &to, amount, gasLimit, gasPrice, data)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	err = ethClient.SendTransaction(context.Background(), transaction)
@@ -102,7 +106,7 @@ func TestSendToNonEnterpriseSmartContractWithProviderSignature(t *testing.T) {
 	gasPrice, err := ethClient.SuggestGasPrice(context.Background())
 	assert.NoError(t, err)
 
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, nil)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, nil)
 	// return newTransaction(nonce, &to, amount, gasLimit, gasPrice, data)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	err = ethClient.SendTransaction(context.Background(), transaction)
@@ -131,7 +135,7 @@ func TestInteractWithNonEnterpriseSmartContractWithoutProviderSignature(t *testi
 
 	// data to interact with a function of this contract
 	dataBytes := []byte("0x552410770000000000000000000000000000000000000000000000000000000000000002")
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, dataBytes)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, dataBytes)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	err = ethClient.SendTransaction(context.Background(), transaction)
 	assert.NoError(t, err)
@@ -157,7 +161,7 @@ func TestSendToEnterPriseSmartContractWithInvalidProviderSignature(t *testing.T)
 	gasPrice, err := ethClient.SuggestGasPrice(context.Background())
 	assert.NoError(t, err)
 
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, nil)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, nil)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	assert.NoError(t, err)
 	transaction, err = types.ProviderSignTx(transaction, signer, ppk)
@@ -186,7 +190,7 @@ func TestSendToEnterPriseSmartContractWithValidProviderSignature(t *testing.T) {
 	gasPrice, err := ethClient.SuggestGasPrice(context.Background())
 	assert.NoError(t, err)
 
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, nil)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, nil)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	assert.NoError(t, err)
 	transaction, err = types.ProviderSignTx(transaction, signer, ppk)
@@ -219,7 +223,7 @@ func TestInteractToEnterpriseSmartContractWithInvalidProviderSignature(t *testin
 
 	// data to interact with a function of this contract
 	dataBytes := []byte("0x552410770000000000000000000000000000000000000000000000000000000000000004")
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, dataBytes)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, dataBytes)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	assert.NoError(t, err)
 	transaction, err = types.ProviderSignTx(transaction, signer, ppk)
@@ -252,7 +256,7 @@ func TestInteractToEnterpriseSmartContractWithValidProviderSignature(t *testing.
 
 	// data to interact with a function of this contract
 	dataBytes := []byte("0x552410770000000000000000000000000000000000000000000000000000000000000004")
-	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(1000000), testGasLimit, gasPrice, dataBytes)
+	transaction := types.NewTransaction(nonce, contractAddr, big.NewInt(testAmountSend), testGasLimit, gasPrice, dataBytes)
 	transaction, err = types.SignTx(transaction, signer, spk)
 	assert.NoError(t, err)
 	transaction, err = types.ProviderSignTx(transaction, signer, ppk)
