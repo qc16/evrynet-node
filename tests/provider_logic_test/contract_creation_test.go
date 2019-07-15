1506345d368168d6c587e763a7081d909a92f633
kyberliem <cyberliem.civil@gmail.com>
2019-07-15 18:00:25 +0700

    add test example on test node

---
+++ tests/provider_logic_test/contract_creation_test.go
@@ -0,0 +1,83 @@
+package provider_logic_test
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+/* These tests are done on a chain with already setup account/ contracts.
+To run these test, please deploy your own account/ contract and extract privatekey inorder to get the expected result
+Adjust these params to match deployment on local machine:
+*/
+
+
+/*
+	Test Send ETH to a normal address
+		- No provider signature is required
+*/
+func TestCreateContractWithProviderAddress(t *testing.T) {
+	spk, err := crypto.HexToECDSA(senderPK)
+	assert.NoError(t, err)
+	sender := common.HexToAddress(senderAddrStr)
+	provideraddr := common.HexToAddress(providerAddrStr)
+	payLoadBytes, err := hexutil.Decode(payload)
+	assert.NoError(t, err)
+
+	ethClient, err := ethclient.Dial(ethRPCEndpoint)
+	assert.NoError(t, err)
+	nonce, err := ethClient.NonceAt(context.Background(), sender, nil)
+	assert.NoError(t, err)
+	tx := types.NewContractCreation(nonce, big.NewInt(0), testGasLimit, big.NewInt(testGasPrice), payLoadBytes, &provideraddr)
+	tx, err = types.SignTx(tx, types.HomesteadSigner{}, spk)
+	assert.NoError(t, err)
+	assert.NoError(t, ethClient.SendTransaction(context.Background(), tx))
+
+}
+
+func TestCreateContractWithoutProviderAddress(t *testing.T) {
+	spk, err := crypto.HexToECDSA(senderPK)
+	assert.NoError(t, err)
+	sender := common.HexToAddress(senderAddrStr)
+	payLoadBytes, err := hexutil.Decode(payload)
+	assert.NoError(t, err)
+
+	ethClient, err := ethclient.Dial(ethRPCEndpoint)
+	assert.NoError(t, err)
+	nonce, err := ethClient.NonceAt(context.Background(), sender, nil)
+	assert.NoError(t, err)
+	tx := types.NewContractCreation(nonce, big.NewInt(0), testGasLimit, big.NewInt(testGasPrice), payLoadBytes, nil)
+	tx, err = types.SignTx(tx, types.HomesteadSigner{}, spk)
+	assert.NoError(t, err)
+	assert.NoError(t, ethClient.SendTransaction(context.Background(), tx))
+
+}
+
+func TestCreateContractWithProviderSignature(t *testing.T) {
+	spk, err := crypto.HexToECDSA(senderPK)
+	assert.NoError(t, err)
+	ppk, err := crypto.HexToECDSA(providerPK)
+	assert.NoError(t, err)
+	sender := common.HexToAddress(senderAddrStr)
+	payLoadBytes, err := hexutil.Decode(payload)
+	assert.NoError(t, err)
+
+	ethClient, err := ethclient.Dial(ethRPCEndpoint)
+	assert.NoError(t, err)
+	nonce, err := ethClient.NonceAt(context.Background(), sender, nil)
+	assert.NoError(t, err)
+	tx := types.NewContractCreation(nonce, big.NewInt(0), testGasLimit, big.NewInt(testGasPrice), payLoadBytes, nil)
+	tx, err = types.SignTx(tx, types.HomesteadSigner{}, spk)
+	assert.NoError(t, err)
+	tx, err = types.ProviderSignTx(tx, types.HomesteadSigner{}, ppk)
+	assert.NoError(t, err)
+	assert.NotEqual(t, nil, ethClient.SendTransaction(context.Background(), tx))
+}
