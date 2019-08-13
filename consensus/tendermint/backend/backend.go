c065c507a3582033362f1718da6536acdb247e9b
kyberliem <cyberliem.civil@gmail.com>
2019-08-13 17:54:32 +0700

    Base backend.Broadcast() and backend.Gossip() outline
    Signed-off-by: kyberliem <cyberliem.civil@gmail.com>
    
    mend

--- consensus/tendermint/backend/backend.go
+++ consensus/tendermint/backend/backend.go
@@ -2,12 +2,18 @@ package backend
 
 import (
 	"crypto/ecdsa"
+	"fmt"
 	"sync"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	tendermintCore "github.com/ethereum/go-ethereum/consensus/tendermint/core"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/event"
+)
+
+const (
+	tendermintMsg = 0x11
 )
 
 // New creates an Ethereum backend for Istanbul core engine.
@@ -25,6 +31,8 @@ type backend struct {
 	tendermintEventMux *event.TypeMux
 	privateKey         *ecdsa.PrivateKey
 	core               tendermintCore.Engine
+	broadcaster        consensus.Broadcaster
+	address            common.Address
 
 	coreStarted bool
 	coreMu      sync.RWMutex
@@ -39,4 +47,49 @@ func (sb *backend) EventMux() *event.TypeMux {
 func (sb *backend) Sign(data []byte) ([]byte, error) {
 	hashData := crypto.Keccak256(data)
 	return crypto.Sign(hashData, sb.privateKey)
+}
+
+// Broadcast implements istanbul.Backend.Broadcast
+// It sends message to its validator by calling gossiping, and send message to itself by eventMux
+// TODO: change AddressSet to validatorSet
+func (sb *backend) Broadcast(addressSet []common.Address, payload []byte) error {
+	// send to others
+	if err := sb.Gossip(addressSet, payload); err != nil {
+		return err
+	}
+	// send to self
+	go func() {
+		if err := sb.tendermintEventMux.Post(payload); err != nil {
+			fmt.Printf("error in Post event %v", err)
+		}
+	}()
+	return nil
+}
+
+// Gossip implements tendermint.Backend.Gossip
+// It sends message to its validators only, not itself.
+// The validators must be able to connected through Peer.
+// TODO: change AddressSet to validatorSet
+func (sb *backend) Gossip(addressSet []common.Address, payload []byte) error {
+	//TODO: check for known message by lru.ARCCache
+
+	targets := make(map[common.Address]bool)
+	for _, val := range addressSet {
+		if val != sb.address {
+			targets[val] = true
+		}
+	}
+
+	if sb.broadcaster != nil && len(targets) > 0 {
+		ps := sb.broadcaster.FindPeers(targets)
+		for _, p := range ps {
+			//TODO: check for recent messsages using lru.ARCCache
+			go func(p consensus.Peer) {
+				if err := p.Send(tendermintMsg, payload); err != nil {
+					fmt.Printf("Error sending message to peer %v", err)
+				}
+			}(p)
+		}
+	}
+	return nil
 }
