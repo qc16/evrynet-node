df96e538b976748b90496a200f2d08928f0d10c0
Mike <manhlx.300695@gmail.com>
2019-08-20 11:17:36 +0700

    (WIP) Add data to store in consensus core for tendermint's consensus

--- consensus/tendermint/core/core.go
+++ consensus/tendermint/core/core.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 
 	"github.com/ethereum/go-ethereum/consensus/tendermint"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
 )
 
@@ -22,6 +24,28 @@ type core struct {
 	backend    tendermint.Backend
 	events     *event.TypeMuxSubscription
 	timeoutSub *event.TypeMuxSubscription
+
+	db ethdb.Database
+
+	height int64
+	round  int
+	block  *types.Block
+
+	valSet tendermint.ValidatorSet // validators set
+
+	lockedRound int          // validator's locked round
+	lockedBlock *types.Block // validator's locked block
+
+	validRound int          // last known round with PoLC for non-nil valid block
+	validBlock *types.Block // last known block of PoLC above
+
+	timeoutProposal *event.TypeMuxSubscription
+	// timeoutPrevote = TimeoutPrevote or TimeoutPrecommit depends on current round step
+	timeoutPrevote *event.TypeMuxSubscription
+
+	proposalReceived   *tendermint.Proposal
+	prevotesReceived   *messageSet
+	precommitsReceived *messageSet
 
 	handlerWg *sync.WaitGroup
 }
