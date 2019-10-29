449b447891dd020a3b5cbc4f6166dab79b2e7761
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2019-10-29 14:14:49 +0700

    update unitest for the apply func

--- consensus/tendermint/backend/snapshot_test.go
+++ consensus/tendermint/backend/snapshot_test.go
@@ -1,6 +1,7 @@
 package backend
 
 import (
+	"bytes"
 	"math/big"
 	"reflect"
 	"testing"
@@ -12,6 +13,7 @@ import (
 	"github.com/evrynet-official/evrynet-client/consensus/tendermint/validator"
 	"github.com/evrynet-official/evrynet-client/core/types"
 	"github.com/evrynet-official/evrynet-client/ethdb"
+	"github.com/evrynet-official/evrynet-client/rlp"
 )
 
 func TestSaveAndLoad(t *testing.T) {
@@ -39,14 +41,16 @@ func TestSaveAndLoad(t *testing.T) {
 
 func TestApplyHeaders(t *testing.T) {
 	var (
-		hash   = common.HexToHash("1234567890")
-		addr1  = common.HexToAddress("1")
-		addr2  = common.HexToAddress("2")
-		addr3  = common.HexToAddress("3")
-		addr4  = common.HexToAddress("4")
-		valSet = validator.NewSet([]common.Address{
+		hash     = common.HexToHash("1234567890")
+		addr1    = common.HexToAddress("1")
+		addr2    = common.HexToAddress("2")
+		addr3    = common.HexToAddress("3")
+		addr4    = common.HexToAddress("4")
+		fakeAddr = common.HexToAddress("99")
+		addrs    = []common.Address{
 			addr1, addr2, addr3, addr4,
-		}, tendermint.RoundRobin, int64(1))
+		}
+		valSet = validator.NewSet(addrs, tendermint.RoundRobin, int64(1))
 
 		//init snapshot with epoch is 5, number is 0
 		snap    = newSnapshot(5, 1, hash, valSet)
@@ -57,49 +61,105 @@ func TestApplyHeaders(t *testing.T) {
 		t.Errorf("validator mismatch: have %v, want %v", val.Address(), addr1)
 	}
 
+	// got error unauthorized with fake proposal
+	headers = createHeaderArr(2, 1, []common.Address{fakeAddr})
+	_, err := snap.apply(headers)
+	assert.Equal(t, err, errUnauthorized)
+
 	//Apply new 2 headers, new proposer should be addr2
-	headers = createHeaderArr(2, 2)
-	snap1, err := snap.apply(headers)
+	headers = createHeaderArr(2, 2, addrs)
+	snap2, err := snap.apply(headers)
 	assert.NoError(t, err)
 
-	if val := snap1.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr3) {
+	if val := snap2.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr3) {
 		t.Errorf("validator mismatch: have %v, want %v", val.Address(), addr3)
 	}
 
 	//Apply new 5 headers, new proposer should be addr2
-	headers = createHeaderArr(2, 5)
-	snap2, err := snap.apply(headers)
+	headers = createHeaderArr(2, 5, addrs)
+	snap3, err := snap.apply(headers)
 	assert.NoError(t, err)
 
-	if val := snap2.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr2) {
+	if val := snap3.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr2) {
 		t.Errorf("validator mismatch: have %v, want %v", val.Address(), addr2)
 	}
 
 	// test save and load after that apply headers
 	db := ethdb.NewMemDatabase()
 	assert.NoError(t, snap.store(db))
-	snap3, err := loadSnapshot(snap.Epoch, db, snap.Hash)
-	if val := snap3.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr1) {
+	snap4, err := loadSnapshot(snap.Epoch, db, snap.Hash)
+	if val := snap4.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr1) {
 		t.Errorf("validator mismatch: have %v, want %v", val.Address(), addr1)
 	}
 
 	//Apply new 9 headers, new proposer should be addr2
-	headers = createHeaderArr(2, 9)
-	snap4, err := snap3.apply(headers)
+	headers = createHeaderArr(2, 9, addrs)
+	snap5, err := snap4.apply(headers)
 	assert.NoError(t, err)
 
-	if val := snap4.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr2) {
+	if val := snap5.ValSet.GetProposer(); !reflect.DeepEqual(val.Address(), addr2) {
 		t.Errorf("validator mismatch: have %v, want %v", val.Address(), addr2)
 	}
 
+	//vote kick out addr1
+	headers = createHeaderArr(2, 4, addrs)
+	applyVotes(headers, addr1, false)
+	snap6, err := snap4.apply(headers)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, snap6.ValSet.Size())
+
+	//vote kick in fakeAddr
+	headers = createHeaderArr(2, 4, addrs)
+	applyVotes(headers, fakeAddr, true)
+	snap7, err := snap4.apply(headers)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, snap7.ValSet.Size())
 }
 
-func createHeaderArr(startNumber int, countHeader int) []*types.Header {
-	headers := make([]*types.Header, countHeader)
-	for i := startNumber; i < startNumber+countHeader; i++ {
-		headers[i-startNumber] = &types.Header{
-			Number: big.NewInt(int64(i)),
+func applyVotes(headers []*types.Header, addr common.Address, vote bool) {
+	for _, header := range headers {
+		extra, _ := prepareExtraWithModified(header, addr)
+		header.Extra = extra
+		if vote {
+			copy(header.Nonce[:], tendermint.NonceAuthVote)
+		} else {
+			copy(header.Nonce[:], tendermint.NonceDropVote)
 		}
 	}
+}
+
+func createHeaderArr(startNumber int, countHeader int, addrs []common.Address) []*types.Header {
+	headers := make([]*types.Header, countHeader)
+	for i := startNumber; i < startNumber+countHeader; i++ {
+		index := i % len(addrs)
+		header := &types.Header{
+			Number:   big.NewInt(int64(i)),
+			Coinbase: addrs[index],
+		}
+		extra, _ := prepareExtra(header)
+		header.Extra = extra
+
+		headers[i-startNumber] = header
+	}
 	return headers
+}
+
+func prepareExtraWithModified(header *types.Header, modifiedAddress common.Address) ([]byte, error) {
+	var buf bytes.Buffer
+
+	// compensate the lack bytes if header.Extra is not enough TendermintExtraVanity bytes.
+	if len(header.Extra) < types.TendermintExtraVanity {
+		header.Extra = append(header.Extra, bytes.Repeat([]byte{0x00}, types.TendermintExtraVanity-len(header.Extra))...)
+	}
+	buf.Write(header.Extra[:types.TendermintExtraVanity])
+
+	tdm := &types.TendermintExtra{
+		ModifiedValidator: modifiedAddress,
+	}
+	payload, err := rlp.EncodeToBytes(&tdm)
+	if err != nil {
+		return nil, err
+	}
+
+	return append(buf.Bytes(), payload...), nil
 }
