fbadd20c538647c4f461dad8de784fe7944bd2a8
Hoang Nguyen <dev.nmhoang@gmail.com>
2019-12-03 14:28:00 +0700

    Fix permission when run docker command
    (cherry picked from commit be786d5b9283ee5cf1c71e908de4689975c78393)

--- deploy/testnet/deploy.sh
+++ deploy/testnet/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ./deploy/testnet/deploy.sh <path_to_share_volumes> <>
 
 localVolumes=$1
@@ -11,8 +11,8 @@ shift
 BASEDIR=$(dirname "$0")
 
 # Remove gev-builder
-docker rmi -f img_builder
-docker rm -f gev-builder
+sudo docker rmi -f img_builder
+sudo docker rm -f gev-builder
 
 # Start builder
 echo "------ Building project ------"
@@ -22,7 +22,19 @@ rm -rf "$BASEDIR"/bootnode/bin
 rm -rf "$BASEDIR"/nodes/bin/gev
 echo "Cloning evrynet-client from develop branch ..."
 git clone --single-branch -b develop --single-branch git@github.com:evrynet-official/evrynet-client.git "$BASEDIR"/builder/project
-yes | docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-builder
+
+# Stop all dockers gracefully to avoid DB crash
+echo "Stop docker containers of nodes"
+"$BASEDIR"/stop_dockers.sh
+
+# Stop bootnode
+sudo docker stop bootnode
+
+# Clear network bridge
+yes | sudo docker network prune
+
+echo "Building builder container"
+yes | sudo docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-builder
 
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]
@@ -31,32 +43,26 @@ then
   echo "=> Building project successfully!"
   sleep 3
 
-  # Stop all dockers gracefully to avoid DB crash
-  echo "------ Stop dockers ------"
-  "$BASEDIR"/stop_dockers.sh
-
-  # Clear network bridge
-  yes | docker network prune
   # Remove old services
-  docker rmi -f img_bootnode img_node_1 img_node_2 img_node_3
-  docker rm -f gev-bootnode gev-node-1 gev-node-2 gev-node-3
+  sudo docker rmi -f img_bootnode img_node_1 img_node_2 img_node_3
+  sudo docker rm -f gev-bootnode gev-node-1 gev-node-2 gev-node-3
 
   echo "Copy gev, bootnode from gev-builder to ./builder/bin/"
   mkdir "$BASEDIR"/builder/bin/
-  docker cp gev-builder:/evrynet/gev "$BASEDIR"/builder/bin/
-  docker cp gev-builder:/evrynet/bootnode "$BASEDIR"/builder/bin/
+  sudo docker cp gev-builder:/evrynet/gev "$BASEDIR"/builder/bin/
+  sudo docker cp gev-builder:/evrynet/bootnode "$BASEDIR"/builder/bin/
 
   # Start bootnode
   echo "------ Start bootnode ------"
   mkdir "$BASEDIR"/bootnode/bin/
   cp "$BASEDIR"/builder/bin/bootnode "$BASEDIR"/bootnode/bin/
-  yes | docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-bootnode
+  yes | sudo docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-bootnode
 
   # Start nodes
   echo "------ Start nodes ------"
   mkdir "$BASEDIR"/nodes/bin/
   cp "$BASEDIR"/builder/bin/gev "$BASEDIR"/nodes/bin/
-  yes | shareVolumes=$localVolumes rpccorsdomain=$rpccorsdomain docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-node-1 gev-node-2 gev-node-3
+  yes | sudo shareVolumes="$localVolumes" rpccorsdomain="$rpccorsdomain" docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-node-1 gev-node-2 gev-node-3
 
   # Start explorer
   # shellcheck disable=SC2039
