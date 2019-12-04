017b3658eb1bd8e936af72b652185b29f0c066cc
Hoang Nguyen <dev.nmhoang@gmail.com>
2019-12-04 18:52:13 +0700

    Update env in docker compose

--- deploy/devnet/node/start_node.sh
+++ deploy/devnet/node/start_node.sh
@@ -3,9 +3,9 @@
 ./gev --datadir ./data init ./genesis.json
 
 # shellcheck disable=SC2039
-if [[ $HAS_METRIC ]]; then
+if [[ $HAS_METRIC == 1 ]]; then
   if [[ ! $METRICS_ENDPOINT ]]; then
-    METRICS_ENDPOINT=http://52.220.52.16:8086
+    METRICS_ENDPOINT=http://127.0.0.1:8086
   fi
   if [[ ! $METRICS_USER ]]; then
     # shellcheck disable=SC2209
@@ -18,7 +18,7 @@ if [[ $HAS_METRIC ]]; then
 
   echo "Start node $NODE_ID with metric!"
   ./gev --datadir ./data --identity "$NODE_ID" --verbosity 4 --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
-    --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*" --rpcport 2200"$NODE_ID" --port 3030"$NODE_ID" \
+    --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*" --rpcport 8545 --port 30303 \
     --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:$BOOTNODE_PORT" --nousb \
     --allow-insecure-unlock \
     --nodekeyhex "$NODEKEYHEX" \
@@ -27,7 +27,7 @@ if [[ $HAS_METRIC ]]; then
 else
   echo "Start node $NODE_ID!"
   ./gev --datadir ./data --identity "$NODE_ID" --verbosity 4 --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
-    --rpc --rpcaddr 0.0.0.0 --rpccorsdomain http://"$RPC_CORSDOMAIN":8080 --rpcvhosts "*" --rpcport 2200"$NODE_ID" --port 3030"$NODE_ID" \
+    --rpc --rpcaddr 0.0.0.0 --rpccorsdomain http://"$RPC_CORSDOMAIN":8080 --rpcvhosts "*" --rpcport 8545 --port 30303 \
     --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:$BOOTNODE_PORT" --nousb \
     --allow-insecure-unlock \
     --nodekeyhex "$NODEKEYHEX" \
