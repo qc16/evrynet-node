0224c36d5756feba3d1d64273d468c0b11598331
Hoang Nguyen <dev.nmhoang@gmail.com>
2019-12-05 11:40:21 +0700

    Update to use pprof flags

--- deploy/testnet/nodes/start_node.sh
+++ deploy/testnet/nodes/start_node.sh
@@ -18,18 +18,20 @@ if [[ $HAS_METRIC ]]; then
 
   echo "Start node $NODE_ID with metric!"
   ./gev --datadir ./data --identity "$NODE_ID" --verbosity 4 --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
-    --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*" --rpcport 2200"$NODE_ID" --port 3030"$NODE_ID" \
+    --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*" --rpcport 8545 --port 30303 \
     --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3 \
     --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30300" \
     --allow-insecure-unlock --unlock "$UNLOCK_ACCOUNT" --password <(echo -n "$UNLOCK_PASS") \
+    --pprof --pprofaddr 0.0.0.0 --pprofport 6060 \
     --nodekeyhex "$NODEKEYHEX"\
     --metrics --metrics.influxdb --metrics.influxdb.endpoint "$METRICS_ENDPOINT" --metrics.influxdb.username $METRICS_USER --metrics.influxdb.password $METRICS_PASS 2>>./log/node.log
 else
   echo "Start node $NODE_ID!"
   ./gev --datadir ./data --identity "$NODE_ID" --verbosity 4 --tendermint.blockperiod 1 --syncmode full --networkid 15 --mine \
-    --rpc --rpcaddr 0.0.0.0 --rpccorsdomain http://"$RPC_CORSDOMAIN":8080 --rpcvhosts "*" --rpcport 2200"$NODE_ID" --port 3030"$NODE_ID" \
+    --rpc --rpcaddr 0.0.0.0 --rpccorsdomain http://"$RPC_CORSDOMAIN":8080 --rpcvhosts "*" --rpcport 8545 --port 30303 \
     --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30300" \
     --allow-insecure-unlock --unlock "$UNLOCK_ACCOUNT" --password <(echo -n "$UNLOCK_PASS") \
+    --pprof --pprofaddr 0.0.0.0 --pprofport 6060 \
     --nodekeyhex "$NODEKEYHEX"\
     --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3 2>>./log/node.log
 fi
