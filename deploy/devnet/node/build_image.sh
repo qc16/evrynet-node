8785d897ba120068543d25e44c2f68927e977033
Thang Dinh <vietthang207@gmail.com>
2019-12-12 17:58:39 +0700

    add build_image.sh

--- deploy/devnet/node/build_image.sh
+++ deploy/devnet/node/build_image.sh
@@ -2,5 +2,7 @@
 echo "------ Buiding Gev-Node docker image ------"
 BASEDIR=$(dirname "$0")
 git clone --single-branch -b develop --single-branch git@github.com:evrynet-official/evrynet-client.git "$BASEDIR"/project
-docker build -f dockerfiles/node/Dockerfile -t kybernetwork/evrynet-node:1.0.1-dev "$BASEDIR"
+pushd "$BASEDIR"/project
+docker build -f dockerfiles/node/Dockerfile -t kybernetwork/evrynet-node:1.0.1-dev .
+popd
 rm -rf "$BASEDIR"/project
