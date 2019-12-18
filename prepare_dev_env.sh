c92e2685963fcad77909c401215b41f4d4856d0b
Hoang Nguyen <dev.nmhoang@gmail.com>
2019-12-18 12:19:07 +0700

    Clean up docker & redundant script

--- prepare_dev_env.sh
+++
@@ -1,8 +0,0 @@
-go build ./cmd/gev
-mkdir ~/evrynet
-cp ./tests/provider_logic_test/test_genesis.json ~/evrynet/genesis.json
-./gev --networkid 15 --datadir ~/evrynet init ~/evrynet/genesis.json
-echo "------------------NOTICE------------------"
-echo "PLEASE PUT 123 AS THE PASSWORD WHEN PROMPT"
-echo "------------------======------------------"
-./gev --datadir ~/evrynet account import tests/provider_logic_test/etherbase_pk.txt
