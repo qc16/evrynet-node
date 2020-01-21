976fb50b95dc8ba4b1925724a295d69dd9b11444
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2020-01-21 17:26:13 +0700

    Write unit test for API endpoint calls

---
+++ tests/provider_logic_test/all_apis_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Evrynetlabs/evrynet-node/rpc"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	RPCEndpoint = "http://52.220.52.16:22001"
+)
+
+func TestAllApis(t *testing.T) {
+
+	rpcClient, err := rpc.Dial(RPCEndpoint)
+	assert.NoError(t, err)
+
+	var (
+		prefix  = "eth_"
+		methods = []string{
+			"protocolVersion", "syncing", "coinbase", "mining", "hashrate", "gasPrice", "accounts",
+			"blockNumber", "getStorageAt", "getTransactionCount", "getBlockTransactionCountByHash",
+			"getBlockTransactionCountByNumber", "getUncleCountByBlockHash", "getUncleCountByBlockNumber",
+			"getCode", "sign", "providerSignTrannsaction", "sendTransaction", "sendRawTransaction",
+			"estimateGas", "getBlockByHash", "getBlockSignerByHash", "getBlockByNumber",
+			"getBlockSignerByNumber", "getTransactionByHash", "getTransactionByBlockHashAndIndex",
+			"getTransactionByBlockNumberAndIndex", "getTransactionReceipt", "getUncleByBlockHashAndIndex",
+			"getCompilers", "compileSolidity", "newFilter",
+			"newBlockFilter", "newPendingTransactionFilter", "uninstallFilter", "getFilterChanges",
+			"getFilterLogs", "getLogs",
+		}
+	)
+
+	for _, element := range methods {
+		if element == "" {
+			continue
+		}
+		method := prefix + element
+		err = rpcClient.CallContext(context.Background(), nil, method)
+		if err != nil {
+			assert.NotContains(t, err.Error(), "the method "+method+" does not exist/is not available")
+		}
+
+		time.Sleep(200 * time.Millisecond)
+	}
+}
