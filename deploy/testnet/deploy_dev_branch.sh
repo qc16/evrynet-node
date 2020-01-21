c710123436da1a37bb2279eaf4601131a662dbf6
Hoang Nguyen <dev.nmhoang@gmail.com>
2020-01-21 16:12:23 +0700

    Update automatic script to deploy Testnet with specific tag from dockerhub

--- deploy/testnet/deploy_dev_branch.sh
+++ deploy/testnet/deploy_dev_branch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ./deploy/testnet/deploy.sh <path_to_share_volumes> <>
+#deploy/testnet/deploy_dev_branch.sh <path_to_share_volumes> <rpc_corsdomain> <deploy_explorer>
 
 localVolumes=$1
 shift
@@ -20,11 +20,12 @@ rm -rf "$BASEDIR"/builder/project
 rm -rf "$BASEDIR"/builder/bin
 rm -rf "$BASEDIR"/bootnode/bin
 rm -rf "$BASEDIR"/nodes/bin/gev
-echo "Cloning evrynet-node from develop branch ..."
+
+echo "--- Cloning evrynet-node from develop branch ..."
 git clone --single-branch -b develop --single-branch git@github.com:Evrynetlabs/evrynet-node.git "$BASEDIR"/builder/project
 
 # Stop all dockers gracefully to avoid DB crash
-echo "Stop docker containers of nodes"
+echo "--- Stop docker containers of nodes"
 "$BASEDIR"/stop_dockers.sh
 
 # Stop bootnode
@@ -33,8 +34,8 @@ sudo docker stop bootnode
 # Clear network bridge
 yes | sudo docker network prune
 
-echo "Building builder container"
-yes | sudo docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-builder
+echo "--- Building builder container"
+yes | sudo version="img_builder" docker-compose -f "$BASEDIR"/docker-compose.yml up -d --force-recreate --build gev-builder
 
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]
@@ -47,7 +48,7 @@ then
   sudo docker rmi -f img_bootnode img_node_1 img_node_2 img_node_3
   sudo docker rm -f gev-bootnode gev-node-1 gev-node-2 gev-node-3
 
-  echo "Copy gev, bootnode from gev-builder to ./builder/bin/"
+  echo "--- Copy gev, bootnode from gev-builder to ./builder/bin/"
   mkdir "$BASEDIR"/builder/bin/
   sudo docker cp gev-builder:/evrynet/gev "$BASEDIR"/builder/bin/
   sudo docker cp gev-builder:/evrynet/bootnode "$BASEDIR"/builder/bin/
@@ -69,7 +70,7 @@ then
   if [[ "$deployExplorer" == "y" ]]; then
     echo "------ Start explorer ------"
     rm -rf "$BASEDIR"/explorer/web
-    echo "Cloning explorer from master branch ..."
+    echo "--- Cloning explorer from master branch ..."
     git clone git@github.com:Evrynetlabs/explorer.git "$BASEDIR"/explorer/web
 
     sudo docker rmi -f img_explorer
@@ -81,6 +82,6 @@ then
   exit 0
 else
   rm -rf "$BASEDIR"/builder/project
-  echo "Building project failed!"
+  echo "--- Building project failed!"
   exit 1
 fi
