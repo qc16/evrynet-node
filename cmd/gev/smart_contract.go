777db0b4cff526584a72831a3a2e6d6a1f3ba0ef
Hoang Nguyen <dev.nmhoang@gmail.com>
2020-02-11 14:40:04 +0700

    Remove redundant code

--- cmd/gev/smart_contract.go
+++ cmd/gev/smart_contract.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -24,40 +21,4 @@ func ReadABIOfSC(path string) (string, error) {
 		return "", err
 	}
 	return string(content), nil
-}
-
-func readSCDir(path string) ([]string, error) {
-	var files []string
-	fileInfo, err := ioutil.ReadDir(path)
-	if err != nil {
-		return files, err
-	}
-
-	for _, info := range fileInfo {
-		if !info.IsDir() && filepath.Ext(info.Name()) == ".sol" {
-			files = append(files, fmt.Sprintf("%s/%s", path, info.Name()))
-		}
-	}
-	return files, nil
-}
-
-func readFirstLine(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if len(line) > 0 {
-			return line, nil //Only read first line
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return "", err
-	}
-	return "", nil
 }
