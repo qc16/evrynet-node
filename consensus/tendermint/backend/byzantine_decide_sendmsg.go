40e3b4d3033c68f1864e26a7279da9ddb9362688
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2020-02-14 14:49:40 +0700

    add log message to reviews byzantine mode

--- consensus/tendermint/backend/byzantine_decide_sendmsg.go
+++ consensus/tendermint/backend/byzantine_decide_sendmsg.go
@@ -13,11 +13,12 @@ func (sb *Backend) checkAndSendMsg(payload []byte) error {
 	if sb.config.FaultyMode == tendermint.RandomlyStopSendingMsg.Uint64() {
 		// randomly stop sending message.
 		switch rand.Intn(2) {
-		case 0:
+		case 0: // stop sending message
 			decidedSendMsg = false
-			log.Warn("Byzantine mode: randomly stop sending message.")
-		case 1:
+			log.Warn("Byzantine mode: stop sending message.")
+		case 1: // sending message
 			decidedSendMsg = true
+			log.Warn("Byzantine mode: sending message.")
 		}
 	}
 
