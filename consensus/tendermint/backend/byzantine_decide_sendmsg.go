2a6a96f31c785779b975023d24f5a047510a00ff
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2020-02-14 12:11:47 +0700

    Byzantine mode randomly stop message sending

---
+++ consensus/tendermint/backend/byzantine_decide_sendmsg.go
@@ -0,0 +1,30 @@
+package backend
+
+import (
+	"math/rand"
+
+	"github.com/Evrynetlabs/evrynet-node/consensus/tendermint"
+	"github.com/Evrynetlabs/evrynet-node/log"
+)
+
+// checkAndSendMsg decided to send the message or not
+func (sb *Backend) checkAndSendMsg(payload []byte) error {
+	var decidedSendMsg = true
+	if sb.config.FaultyMode == tendermint.RandomlyStopSendingMsg.Uint64() {
+		// randomly stop sending message.
+		switch rand.Intn(2) {
+		case 0:
+			decidedSendMsg = false
+			log.Warn("Byzantine mode: randomly stop sending message.")
+		case 1:
+			decidedSendMsg = true
+		}
+	}
+
+	if decidedSendMsg {
+		return sb.EventMux().Post(tendermint.MessageEvent{
+			Payload: payload,
+		})
+	}
+	return nil
+}
