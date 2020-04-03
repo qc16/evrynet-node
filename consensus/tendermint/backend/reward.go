08eba03d22667a4382bd9a53718853af958ee086
ducquangkstn <ducquang.kstn@gmail.com>
2020-04-03 11:14:15 +0700

    refactor as request

---
+++ consensus/tendermint/backend/reward.go
@@ -0,0 +1,122 @@
+package backend
+
+import (
+	"math/big"
+	"time"
+
+	"github.com/Evrynetlabs/evrynet-node/common"
+	"github.com/Evrynetlabs/evrynet-node/consensus"
+	"github.com/Evrynetlabs/evrynet-node/consensus/tendermint"
+	"github.com/Evrynetlabs/evrynet-node/consensus/tendermint/utils"
+	"github.com/Evrynetlabs/evrynet-node/core/state"
+	"github.com/Evrynetlabs/evrynet-node/core/state/staking"
+	"github.com/Evrynetlabs/evrynet-node/core/types"
+	"github.com/Evrynetlabs/evrynet-node/core/vm"
+	"github.com/Evrynetlabs/evrynet-node/log"
+)
+
+// AccumulateRewards credits the coinbase of the given block with the proposing
+// reward.
+func (sb *Backend) accumulateRewards(chainReader consensus.ChainReader, state *state.StateDB, header *types.Header) error {
+	// If fixed validators (test) then return
+	if chainReader.Config().Tendermint.FixedValidators != nil {
+		reward := new(big.Int).Set(TendermintBlockReward)
+		state.AddBalance(header.Coinbase, reward)
+		return nil
+	}
+	var (
+		currentBlock = header.Number.Uint64()
+		epoch        = chainReader.Config().Tendermint.Epoch
+		start        = time.Now()
+	)
+
+	if currentBlock == 0 {
+		return tendermint.ErrFinalizeZeroBlock
+	}
+
+	if currentBlock%epoch != 0 {
+		return nil
+	}
+
+	validatorsRewards := calculateTotalValidatorsRewards(chainReader, epoch, header)
+	transitionHeader := chainReader.GetHeaderByNumber(currentBlock - epoch)
+	validatorAdds, err := utils.GetValSetAddresses(transitionHeader)
+	if err != nil {
+		return err
+	}
+	stateDB, err := sb.chain.StateAt(transitionHeader.Root)
+	if err != nil {
+		return err
+	}
+	stakingCaller := staking.NewStakingCaller(stateDB, staking.NewChainContextWrapper(sb, sb.chain.GetHeader), transitionHeader, sb.chain.Config(), vm.Config{})
+	validatorsData, err := stakingCaller.GetValidatorsData(*sb.config.StakingSCAddress, validatorAdds)
+	if err != nil {
+		return err
+	}
+
+	finalReward := calculateReward(validatorsData, validatorsRewards)
+	for addr, value := range finalReward {
+		state.AddBalance(addr, value)
+	}
+	log.Debug("accumulateRewards", "number", currentBlock, "elapsed", common.PrettyDuration(time.Since(start)))
+	return nil
+}
+
+// calculateTotalValidatorsRewards gets reward from chainReader and current header (from finalize)
+// reward includes block rewards and tx fee from block number currentBlock - epoch +1
+func calculateTotalValidatorsRewards(chainReader consensus.ChainReader, epoch uint64, header *types.Header) map[common.Address]*big.Int {
+	var currentBlock = header.Number.Uint64()
+	validatorsRewards := make(map[common.Address]*big.Int)
+	addReward := func(addr common.Address, value *big.Int) {
+		if current, ok := validatorsRewards[addr]; ok {
+			validatorsRewards[addr] = new(big.Int).Add(current, value)
+		} else {
+			validatorsRewards[addr] = value
+		}
+	}
+	for i := currentBlock - epoch + 1; i <= currentBlock; i++ {
+		var currentHeader *types.Header
+		if i != currentBlock {
+			currentHeader = chainReader.GetHeaderByNumber(i)
+		} else {
+			currentHeader = header
+		}
+		reward := new(big.Int).Set(TendermintBlockReward)
+		txFee := new(big.Int).Mul(big.NewInt(int64(currentHeader.GasUsed)), chainReader.Config().GasPrice)
+		addReward(currentHeader.Coinbase, new(big.Int).Add(reward, txFee))
+	}
+	return validatorsRewards
+}
+
+// calculateReward divides rewards into 50% to owner and 50% among voters
+// rewards for voters is proportional to voters'stake
+func calculateReward(validatorsData map[common.Address]staking.CandidateData, validatorsReward map[common.Address]*big.Int) map[common.Address]*big.Int {
+	finalReward := make(map[common.Address]*big.Int)
+	addReward := func(addr common.Address, value *big.Int) {
+		if current, ok := finalReward[addr]; ok {
+			finalReward[addr] = new(big.Int).Add(current, value)
+		} else {
+			finalReward[addr] = new(big.Int).Set(value)
+		}
+	}
+	for addr, validatorData := range validatorsData {
+		totalReward, ok := validatorsReward[addr]
+		if !ok {
+			continue
+		}
+		// remainingReward to ensure the total reward for the voters and owner is equals to the wei validator earns
+		remainingReward := new(big.Int).Set(totalReward)
+		totalVoterReward := new(big.Int).Mul(totalReward, big.NewInt(voterRewardPercentage))
+		totalVoterReward = new(big.Int).Div(totalVoterReward, big.NewInt(100))
+		for voter, voterStake := range validatorData.VoterStakes {
+			voterReward := new(big.Int).Mul(totalVoterReward, voterStake)
+			voterReward = new(big.Int).Div(voterReward, validatorData.TotalStake)
+			addReward(voter, voterReward)
+			remainingReward.Sub(remainingReward, voterReward)
+		}
+		validatorReward := new(big.Int).Mul(totalReward, big.NewInt(validatorRewardPercentage))
+		validatorReward = new(big.Int).Div(validatorReward, big.NewInt(100))
+		addReward(validatorData.Owner, remainingReward)
+	}
+	return finalReward
+}
