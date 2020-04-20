2044f616abd5259b84fbfff158906bff4e3b2319
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2020-04-20 18:11:16 +0700

    fix bug the unittest and remove hardcode as request

--- consensus/clique/clique_test.go
+++ consensus/clique/clique_test.go
@@ -36,6 +36,10 @@ import (
 // already in the database. The bug was that processing the block *prior* to an
 // empty one **also completes** the empty one, ending up in a known-block error.
 func TestReimportMirroredState(t *testing.T) {
+	const (
+		// the magic number to initialize the balance of an addr.
+		magicNumberForBalance = 100000000
+	)
 	// Initialize a Clique chain with a single signer
 	var (
 		db     = rawdb.NewMemoryDatabase()
@@ -47,7 +51,7 @@ func TestReimportMirroredState(t *testing.T) {
 	genspec := &core.Genesis{
 		ExtraData: make([]byte, extraVanity+common.AddressLength+extraSeal),
 		Alloc: map[common.Address]core.GenesisAccount{
-			addr: {Balance: new(big.Int).Mul(big.NewInt(100000000), big.NewInt(params.GasPriceConfig))},
+			addr: {Balance: new(big.Int).Mul(big.NewInt(magicNumberForBalance), big.NewInt(params.GasPriceConfig))},
 		},
 	}
 	copy(genspec.ExtraData[extraVanity:], addr[:])
